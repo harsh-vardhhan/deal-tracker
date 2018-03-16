50ba85748c587fd05c4f5294cdfa05b04ef4df8a
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2018-03-16 00:06:29 +0000

    grommet type definition added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+//@flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../src/grommet/grommet-hpe.min.css';
 import App from './containers/App';
 
+//$FlowFixMe
 ReactDOM.render(<App/>, document.getElementById('root'));
