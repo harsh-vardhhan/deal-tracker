ecb9a9f8f61c5acb896fbbb8366d793b120b8b4e
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2018-03-20 00:31:18 +0000

    mock deals data

---
+++ src/mock/deals.js
@@ -0,0 +1,18 @@
+//@flow
+export const dealsMock = [
+    {
+        id: 1,
+        name: 'Discount on Broadband',
+        price: 12.45
+    },
+    {
+        id: 2,
+        name: 'Discount on Telephone',
+        price: 32.75
+    },
+    {
+        id: 3,
+        name: 'Discount on Landline',
+        price: 16.25
+    }
+];
