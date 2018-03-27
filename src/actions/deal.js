5492e0efb45565b4e8307c7318a9ef0f6a5fea65
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2018-03-28 00:31:43 +0100

    remove return from actions

--- src/actions/deal.js
+++ src/actions/deal.js
@@ -10,27 +10,27 @@ import type {
     EDIT_DEAL_ACTION
 } from '../types/Action';
 
-export const getDeals = () => {
-    return async (dispatch: Dispatch<GET_DEALS_ACTION>) => {
+export const getDeals = () => (
+    async (dispatch: Dispatch<GET_DEALS_ACTION>) => {
         const deals = await getDealsMock();
         dispatch({type: GET_DEALS, deals});
-    };
-};
+    }
+);
 
-export const addDeal = (deal: Deal) => {
-    return async (dispatch: Dispatch<ADD_DEALS_ACTION>) => {
+export const addDeal = (deal: Deal) => (
+    async (dispatch: Dispatch<ADD_DEALS_ACTION>) => {
         dispatch({type: ADD_DEAL, deal});
-    };
-};
+    }
+);
 
-export const deleteDeal = (dealId: number) => {
-    return async (dispatch: Dispatch<DELETE_DEAL_ACTION>) => {
+export const deleteDeal = (dealId: number) => (
+    async (dispatch: Dispatch<DELETE_DEAL_ACTION>) => {
         dispatch({type: DELETE_DEAL, dealId});
-    };
-};
+    }
+);
 
-export const editDeal = (deal: Deal, dealId: number) => {
-    return async (dispatch: Dispatch<EDIT_DEAL_ACTION>) => {
+export const editDeal = (deal: Deal, dealId: number) => (
+    async (dispatch: Dispatch<EDIT_DEAL_ACTION>) => {
         dispatch({type: EDIT_DEAL, deal, dealId});
-    };
-};
+    }
+);
