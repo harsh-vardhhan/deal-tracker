76bc43b02f84cc9707bf02538eb04e8bdc366c54
trakbit <harsh.vardhan.611@gmail.com>
2018-05-06 14:42:33 +0100

    Remove console.log

--- src/api/deal.js
+++ src/api/deal.js
@@ -5,10 +5,7 @@ import type {Deals} from '../types/Deals';
 const coffeeUrl = 'http://localhost:5000/api/deals';
 
 export const getDealsData = () => {
-    const success = ({data}: {data: Deals}) => {
-        console.log(data);
-        return data;
-    };
+    const success = ({data}: {data: Deals}) => data;
     const error = () => [];
     return axios.get(coffeeUrl).then(success).catch(error);
 };
