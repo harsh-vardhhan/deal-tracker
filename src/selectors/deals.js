7968793976393054f0e6a866c1fb6432eeb45e93
trakbit <harsh.vardhan.611@gmail.com>
2018-05-14 11:22:19 +0100

    remove return from selectors

--- src/selectors/deals.js
+++ src/selectors/deals.js
@@ -2,13 +2,8 @@
 import {createSelector} from 'reselect';
 import type {State} from '../types/State';
 
-const dealSelector = (state: State) => {
-    return state.dealReducer;
-};
-
-const dealNameSelector = (state: State) => {
-    return state.dealSearchReducer;
-};
+const dealSelector = (state: State) => state.dealReducer;
+const dealNameSelector = (state: State) => state.dealSearchReducer;
 
 export const filterDeals = createSelector(dealSelector, dealNameSelector,
     (dealReducer, dealSearchReducer) => dealReducer.filter((item) => item.name.includes(dealSearchReducer)));
