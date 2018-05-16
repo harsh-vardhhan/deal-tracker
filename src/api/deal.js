d57a3e15807399d5f5b900950f0334ee3645e9f7
trakbit <harsh.vardhan.611@gmail.com>
2018-05-16 16:12:07 +0100

    heroku URL

--- src/api/deal.js
+++ src/api/deal.js
@@ -3,7 +3,7 @@ import {dealsMock} from '../mock/deals.js';
 import axios from 'axios';
 import type {Deals} from '../types/Deals';
 import type {Deal} from '../types/Deal';
-const DealUrl = 'http://localhost:5000/api/deals';
+const DealUrl = 'https://hidden-anchorage-11098.herokuapp.com/api/deals';
 
 export const getDealsData = () => {
     const success = ({data}: {data: Deals}) => data;
