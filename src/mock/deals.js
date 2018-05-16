390e2c9184c675b26959560a9aa8013893dd6d97
trakbit <harsh.vardhan.611@gmail.com>
2018-05-16 16:49:45 +0100

    static version

--- src/mock/deals.js
+++ src/mock/deals.js
@@ -1,25 +1,25 @@
 //@flow
 export const dealsMock = [
     {
-        id: 0,
+        _id: '0',
         name: 'Discount on Broadband',
         amount: 12.45,
         stage: 1
     },
     {
-        id: 1,
+        _id: '1',
         name: 'Discount on Telephone',
         amount: 32.75,
         stage: 3
     },
     {
-        id: 2,
+        _id: '2',
         name: 'Discount on Landline',
         amount: 16.25,
         stage: 2
     },
     {
-        id: 3,
+        _id: '3',
         name: 'Discount on Internet',
         amount: 16.25,
         stage: 2
