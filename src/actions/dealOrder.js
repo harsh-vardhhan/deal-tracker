2f5cfddfe12d00544d13c0709f0134121e1d8101
trakbit <harsh.vardhan.611@gmail.com>
2018-05-19 14:46:11 +0100

    TOGGLE ORDER constant

--- src/actions/dealOrder.js
+++ src/actions/dealOrder.js
@@ -3,9 +3,10 @@ import type {
     TOGGLE_ORDER_ACTION
 } from '../types/Action';
 import type {Dispatch} from 'redux';
+import {TOGGLE_ORDER} from '../constants/deals';
 
 export const toggleDealOrder = () => (
     async (dispatch: Dispatch<TOGGLE_ORDER_ACTION>) => {
-        dispatch({type: 'TOGGLE_ORDER'});
+        dispatch({type: TOGGLE_ORDER});
     }
 );
